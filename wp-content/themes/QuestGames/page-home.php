da354f82bc9dee320c95bffa69dfd58f130e535e
Alessandro-Massarotti-jr <alemassajr@hotmail.com>
2022-03-17 10:55:41 -0300

    ajustes projeto

--- wp-content/themes/QuestGames/page-home.php
+++ wp-content/themes/QuestGames/page-home.php
@@ -20,41 +20,48 @@ $banner = new WP_Query($bannerArgs);
 ?>
 
 <?php get_header(); ?>
-<div class="bannerContainer">
-    <div class="banner" id="bannerHome">
-        <?php if ($banner->have_posts()) : while ($banner->have_posts()) : $banner->the_post() ?>
-                <a href="<?php echo the_permalink() ?>" class="bannerItem">
-                <h2 class="bannerTitle"><?php the_title();?></h2>
-                    <div class="bannerItemImage">
-                        <img src="<?php the_post_thumbnail_url('post_image') ?>" alt="<?php the_title() ?>">
-                    </div>
-                </a>
-        <?php endwhile;
-        endif; ?>
-    </div>
-    <div class="bannerNav">
-        <label for="bannerNav1">
-            <input class="BannerNavBtn" type="radio" name="radio-btn" id="bannerNav1" onchange="banner1()">
-            <span></span>
-        </label>
-        <label for="bannerNav2">
-            <input class="BannerNavBtn" type="radio" name="radio-btn" id="bannerNav2" onchange="banner2()">
-            <span></span>
-        </label>
-        <label for="bannerNav3"> 
-            <input class="BannerNavBtn" type="radio" name="radio-btn" id="bannerNav3" onchange="banner3()">
-            <span></span>
-        </label>
-    </div>
-</div>
 
-<?php if ($games->have_posts()) : while ($games->have_posts()) : $games->the_post() ?>
-        <div style="background-color: white; margin-top:40px;">
-            <img src="<?php the_post_thumbnail_url('post_image') ?>" alt="<?php the_title() ?>">
-            <h1><?php the_title(); ?></h1>
-            <a href="<?php echo the_permalink() ?>">Ver mais</a>
+<div class="home">
+
+
+
+    <div class="bannerContainer">
+        <div class="banner" id="bannerHome">
+            <?php if ($banner->have_posts()) : while ($banner->have_posts()) : $banner->the_post() ?>
+                    <a href="<?php echo the_permalink() ?>" class="bannerItem">
+                        <h2 class="bannerTitle"><?php the_title(); ?></h2>
+                        <div class="bannerItemImage">
+                            <img src="<?php the_post_thumbnail_url('post_image') ?>" alt="<?php the_title() ?>">
+                        </div>
+                    </a>
+            <?php endwhile;
+            endif; ?>
         </div>
-<?php endwhile;
-endif; ?>
+        <div class="bannerNav">
+            <label for="bannerNav1">
+                <input class="BannerNavBtn" type="radio" name="radio-btn" id="bannerNav1" onchange="banner1()">
+                <span></span>
+            </label>
+            <label for="bannerNav2">
+                <input class="BannerNavBtn" type="radio" name="radio-btn" id="bannerNav2" onchange="banner2()">
+                <span></span>
+            </label>
+            <label for="bannerNav3">
+                <input class="BannerNavBtn" type="radio" name="radio-btn" id="bannerNav3" onchange="banner3()">
+                <span></span>
+            </label>
+        </div>
+    </div>
+
+    <?php if ($games->have_posts()) : while ($games->have_posts()) : $games->the_post() ?>
+            <div style="background-color: white; margin-top:40px;">
+                <img src="<?php the_post_thumbnail_url('post_image') ?>" alt="<?php the_title() ?>">
+                <h1><?php the_title(); ?></h1>
+                <a href="<?php echo the_permalink() ?>">Ver mais</a>
+            </div>
+    <?php endwhile;
+    endif; ?>
+
+</div>
 
 <?php get_footer(); ?>
