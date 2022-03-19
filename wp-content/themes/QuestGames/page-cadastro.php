59964bfc0a90309b630e8cc315c821130465c5f7
Alessandro-Massarotti-jr <alemassajr@hotmail.com>
2022-03-19 16:57:17 -0300

    pagina de cadastro feita

--- wp-content/themes/QuestGames/page-cadastro.php
+++ wp-content/themes/QuestGames/page-cadastro.php
@@ -7,18 +7,26 @@ Template Name: Cadastro
 <?php get_header() ?>
 
 <style>
-    header, footer, .footerlinks{
+    header,
+    footer,
+    .footerlinks {
         display: none;
     }
-    #main{
+
+    #main {
         margin-left: 0;
     }
-   </style>
+</style>
 
 
 <div class="cadastro">
+    <div class="image">
     <img src="<?php echo get_template_directory_uri() . '/assets/image/Flogin.png' ?>" alt="Imagem Login">
-    <?php echo do_shortcode('[user_registration_form id="35"]') ?>
+    </div>
+    <div class="form">
+        <?php echo do_shortcode('[user_registration_form id="35"]') ?>
+        <a class="homeBtn" href="<?php echo home_url()?>"><ion-icon name="arrow-back-outline"></ion-icon> Voltar para a Home</a>
+    </div>
 </div>
 
 <?php get_footer() ?>
