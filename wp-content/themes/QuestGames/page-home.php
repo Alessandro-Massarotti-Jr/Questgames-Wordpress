39f25ca969c5dffa149963d574c042bed2c23c2b
Alessandro-Massarotti-jr <alemassajr@hotmail.com>
2022-06-16 14:27:56 -0300

    change header

--- wp-content/themes/QuestGames/page-home.php
+++ wp-content/themes/QuestGames/page-home.php
@@ -40,32 +40,7 @@ $all_categories = get_categories($categoriesArgs);
 
 <div class="home">
 
-    <div class="head">
-        <a href="<?php echo home_url() ?>">
-            <ion-icon class="icons" name="bag-add-outline"></ion-icon>
-        </a>
-        <a href="<?php echo home_url() . '/carrinho' ?>">
-            <ion-icon class="icons" name="cart-outline"></ion-icon>
-        </a>
-        <a href="<?php echo home_url() . '/categorias' ?>">
-            <ion-icon class="icons" name="pricetag-outline"></ion-icon>
-        </a>
-        <?php if (is_user_logged_in()) : ?>
-            <a href="<?php echo home_url() . '/biblioteca/' ?>">
-                <ion-icon class="icons" name="library-outline"></ion-icon>
-            </a>
-            <a href="<?php echo home_url() . '/perfil' ?>">
-                <ion-icon class="icons" name="person-outline"></ion-icon>
-            </a>
-        <?php else : ?>
-            <a href="<?php echo home_url() ?>/login">
-                <ion-icon class="icons" name="log-in-outline"></ion-icon>
-            </a>
-            <a href="<?php echo home_url() . '/cadastro' ?>">
-                <ion-icon class="icons" name="person-add-outline"></ion-icon>
-            </a>
-        <?php endif; ?>
-    </div>
+  
 
     <div class="bannerContainer">
         <div class="banner" id="bannerHome">
--- wp-content/themes/QuestGames/page-login.php
+++ wp-content/themes/QuestGames/page-login.php
@@ -17,32 +17,6 @@ Template Name: login
         margin-left: 0;
     }
 </style>
-<div class="head">
-        <a href="<?php echo home_url() ?>">
-            <ion-icon class="icons" name="bag-add-outline"></ion-icon>
-        </a>
-        <a href="<?php echo home_url() . '/carrinho' ?>">
-            <ion-icon class="icons" name="cart-outline"></ion-icon>
-        </a>
-        <a href="<?php echo home_url() . '/categorias' ?>">
-            <ion-icon class="icons" name="pricetag-outline"></ion-icon>
-        </a>
-        <?php if (is_user_logged_in()) : ?>
-            <a href="<?php echo home_url() . '/biblioteca/' ?>">
-                <ion-icon class="icons" name="library-outline"></ion-icon>
-            </a>
-            <a href="<?php echo home_url() . '/perfil' ?>">
-                <ion-icon class="icons" name="person-outline"></ion-icon>
-            </a>
-        <?php else : ?>
-            <a href="<?php echo home_url() ?>/login">
-                <ion-icon class="icons" name="log-in-outline"></ion-icon>
-            </a>
-            <a href="<?php echo home_url() . '/cadastro' ?>">
-                <ion-icon class="icons" name="person-add-outline"></ion-icon>
-            </a>
-        <?php endif; ?>
-    </div>
 <div class="login">
     <div class="image">
         <img src="<?php echo get_template_directory_uri() . '/assets/image/Flogin.png' ?>" alt="Imagem Login">
